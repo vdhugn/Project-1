20cc30ae3f1c2313efa16234e77b38e192159340
hhiep1504 <114068499+hhiep1504@users.noreply.github.com>
2024-01-21 23:27:11 +0700

    update code and requirements.txt

--- Binary-Search-Tree/GUI/gui.py
+++ Binary-Search-Tree/GUI/gui.py
@@ -3,94 +3,17 @@ from tkinter.messagebox import showerror
 from tkinter import font
 from bst.bstVizualize import *
 
-def createRectangleWithText(canvas, centerX, centerY, width, height, rectangleColor, text, textColor, fontSize):
-    canvas.create_rectangle(centerX - width / 2, centerY - height / 2,
-                            centerX + width / 2, centerY + height / 2,
-                            fill=rectangleColor, width=0)
-    canvas.create_text(centerX, centerY,
-                       text=text, fill=textColor, font=("Arial " + str(int(fontSize)) + " bold"))
-
-
-def isInputValid(value) -> bool:
+def isInputValid(value):
     try:
         value = int(value)
     except ValueError:
         showerror(title="ERROR", message="Invalid input")
         return False
-    if value > MAX_VALUE:
-        showerror(title="ERROR", message="Input value exceeding max allowed")
-        return False
-    if value < MIN_VALUE:
-        showerror(title="ERROR", message="Input value under min allowed")
+    
+    if not (minValue <= value <= maxValue):
+        showerror(title="ERROR", message="Input value out of range")
         return False
     return True
-
-
-def onClickInsert(value):
-    global rootNode
-    values = [val.strip() for val in value.split(',')]
-    for val in values:
-        if not isInputValid(val):
-            return
-        val = int(val)
-        root_x = WINDOW_WIDTH / 2
-        root_y = Y_PADDING
-        disableUI()
-        rootNode = insertNode(rootNode, val, root_x, root_y, 0, canvas, window)
-        sleep(1)
-        canvas.delete("all")
-        drawTree(rootNode, root_x, root_y, 0, canvas, window)
-    enableUI()
-
-
-def onClickSearch(value):
-    if not isInputValid(value):
-        return
-    value = int(value)
-    rootPositionX = WINDOW_WIDTH/2
-    rootPositionY = Y_PADDING
-    disableUI()
-    searchTree(rootNode, value, rootPositionX, rootPositionY, 0, canvas, window)
-    sleep(1)
-    canvas.delete("all")
-    drawTree(rootNode, rootPositionX, rootPositionY, 0, canvas, window)   
-    enableUI()
-
-
-def onClickDelete(value):
-    global rootNode
-    if not isInputValid(value):
-        return
-    value = int(value)
-    rootPositionX = WINDOW_WIDTH/2
-    rootPositionY = Y_PADDING
-    disableUI()
-    rootNode = deleteNode(rootNode, value, rootPositionX, rootPositionY, 0, canvas, window)
-    sleep(1)
-    canvas.delete("all")
-    drawTree(rootNode, rootPositionX, rootPositionY, 0, canvas, window)   
-    enableUI()
-
-
-def onClickGenerateRandomTree():
-    global rootNode
-    rootNode = None
-    numberOfInserts = randint(100, 100)
-    for x in range(numberOfInserts):
-        nodeValue = randint(MIN_VALUE, MAX_VALUE)
-        rootNode = insertNodeWithoutAnimation(rootNode, nodeValue, 0)   
-    rootPositionX = WINDOW_WIDTH/2
-    rootPositionY = Y_PADDING
-    canvas.delete("all")
-    drawTree(rootNode, rootPositionX, rootPositionY, 0, canvas, window)
-
-def onClickClear():
-    global rootNode
-    disableUI()
-    rootNode = None
-    canvas.delete("all")
-    enableUI()
-
 
 def disableUI():
     insertButton["state"] = DISABLED
@@ -106,30 +29,98 @@ def enableUI():
     deleteButton["state"] = NORMAL
     searchButton["state"] = NORMAL
     inputField["state"] = NORMAL
+    
 
+def InsertButtonClick(value):
+    global rootNode
+    values = [val.strip() for val in value.split(',')]
+    for val in values:
+        if not isInputValid(val):
+            return
+        val = int(val)
+        root_x = window_width / 2
+        root_y = y_padding
+        disableUI()
+        rootNode = insertNode(rootNode, val, root_x, root_y, 0, canvas, window)
+        sleep(1)
+        canvas.delete("all")
+        drawTree(rootNode, root_x, root_y, 0, canvas, window)
+    enableUI()
+
+
+def SearchButtonClick(value):
+    if not isInputValid(value):
+        return
+    value = int(value)
+    rootPositionX = window_width/2
+    rootPositionY = y_padding
+    disableUI()
+    searchTree(rootNode, value, rootPositionX, rootPositionY, 0, canvas, window)
+    sleep(1)
+    canvas.delete("all")
+    drawTree(rootNode, rootPositionX, rootPositionY, 0, canvas, window)   
+    enableUI()
+
+
+def DeleteButtonClick(value):
+    global rootNode
+    if not isInputValid(value):
+        return
+    value = int(value)
+    rootPositionX = window_width/2
+    rootPositionY = y_padding
+    disableUI()
+    rootNode = deleteNode(rootNode, value, rootPositionX, rootPositionY, 0, canvas, window)
+    sleep(1)
+    canvas.delete("all")
+    drawTree(rootNode, rootPositionX, rootPositionY, 0, canvas, window)   
+    enableUI()
+
+
+def GenerateRandomTreeButtonClick():
+    global rootNode
+    rootNode = None
+    numberOfInserts = randint(100, 100)
+    for x in range(numberOfInserts):
+        nodeValue = randint(minValue, maxValue)
+        rootNode = insertNode_(rootNode, nodeValue, 0)   
+    rootPositionX = window_width/2
+    rootPositionY = y_padding
+    canvas.delete("all")
+    drawTree(rootNode, rootPositionX, rootPositionY, 0, canvas, window)
+
+def ClearButtonClick():
+    global rootNode
+    disableUI()
+    rootNode = None
+    canvas.delete("all")
+    enableUI()
+
+
+# SET UP UI
 button_font = font.Font(family="Tektur", size=14, weight="bold")
 
 inputField = Entry(window, font = font.Font(family="Tektur", size=12))
 inputField.pack(side=LEFT, expand=1)
 
 generateRandomTreeButton = Button(window, text="Generate Random Tree", font=button_font, 
-                                  command=lambda: onClickGenerateRandomTree())
+                                  command=lambda: GenerateRandomTreeButtonClick())
 generateRandomTreeButton.pack(side=LEFT, fill=X, expand=1)
 
-insertButton = Button(window, text="Insert", font=button_font, command=lambda: onClickInsert(inputField.get()))
+insertButton = Button(window, text="Insert", font=button_font, command=lambda: InsertButtonClick(inputField.get()))
 insertButton.pack(side=LEFT, fill=X, expand=1)
 
-deleteButton = Button(window, text="Delete", font=button_font, command=lambda: onClickDelete(inputField.get()))
+deleteButton = Button(window, text="Delete", font=button_font, command=lambda: DeleteButtonClick(inputField.get()))
 deleteButton.pack(side=LEFT, fill=X, expand=1)
 
-searchButton = Button(window, text="Search", font=button_font, command=lambda: onClickSearch(inputField.get()))
+searchButton = Button(window, text="Search", font=button_font, command=lambda: SearchButtonClick(inputField.get()))
 searchButton.pack(side=LEFT, fill=X, expand=1)
 
-clearButton = Button(window, text="Clear", font=button_font, command=onClickClear)
+clearButton = Button(window, text="Clear", font=button_font, command=ClearButtonClick)
 clearButton.pack(side=LEFT, fill=X, expand=1)
 
 def generate():
-    window.geometry(str(WINDOW_WIDTH) + "x" + str(WINDOW_HEIGHT) + "+100-100")
+    window.geometry(str(window_width) + "x" + str(window_height) + "+100-100")
     window.resizable(True, True)
     window.title("Binary Search Tree")
     window.mainloop()
--- Binary-Search-Tree/bst/bstVizualize.py
+++ Binary-Search-Tree/bst/bstVizualize.py
@@ -5,7 +5,7 @@ from bst.constant import *
 
 rootNode = None
 window = Tk()
-canvas = Canvas(window, bg=BACKGROUND_COLOR)
+canvas = Canvas(window, bg=background_color)
 canvas.pack(side='top', fill='both', expand=2)
 
 class Node:
@@ -15,19 +15,22 @@ class Node:
         self.rightChild = None
 
 def calculate_left_child_pos(parent_x, parent_y, childDepth):
-    leftChild_x = parent_x - ((WINDOW_WIDTH - X_PADDING) / pow(2, childDepth)) / 2
-    leftChild_y = parent_y + NODE_RADIUS * 4
+    leftChild_x = parent_x - ((window_width - x_padding) / pow(2, childDepth)) / 2
+    leftChild_y = parent_y + node_radius * 4
     return (leftChild_x, leftChild_y)
 
 
 def calculate_right_child_pos(parent_x, parent_y, childDepth):
-    rightChild_x = parent_x + ((WINDOW_WIDTH - X_PADDING) / pow(2, childDepth)) / 2
-    rightChild_y = parent_y + NODE_RADIUS * 4
+    rightChild_x = parent_x + ((window_width - x_padding) / pow(2, childDepth)) / 2
+    rightChild_y = parent_y + node_radius * 4
     return (rightChild_x, rightChild_y)
 
-
+def oval_with_text(canvas, centerX, centerY, radius, ovalColor, text, textColor, fontSize):
+    oval = canvas.create_oval(centerX - radius, centerY - radius, centerX + radius, centerY + radius, fill=ovalColor, width=0)
+    text = canvas.create_text(centerX, centerY, text=text, fill=textColor, font=("Calibri " + str(int(fontSize)) + " bold"))
+    
 def insertNode(rootNode, value, root_x, root_y, nodeDepth, canvas, window):
-    if nodeDepth > MAX_DEPTH:
+    if nodeDepth > maxDepth:
         showinfo(title="Insert", message="Max depth reached")
         return rootNode
 
@@ -36,27 +39,28 @@ def insertNode(rootNode, value, root_x, root_y, nodeDepth, canvas, window):
         return rootNode
     
     if value < rootNode.value:
-        leftChildPositionX, leftChildPositionY = calculate_left_child_pos(root_x, root_y, nodeDepth + 1)
-        rootNode.leftChild = insertNode(rootNode.leftChild, value, leftChildPositionX, leftChildPositionY, nodeDepth + 1, canvas, window)
+        leftChild_X, leftChild_Y = calculate_left_child_pos(root_x, root_y, nodeDepth + 1)
+        rootNode.leftChild = insertNode(rootNode.leftChild, value, leftChild_X, leftChild_Y, nodeDepth + 1, canvas, window)
     elif value > rootNode.value:
-        rightChildPositionX, rightChildPositionY = calculate_right_child_pos(root_x, root_y, nodeDepth + 1)
-        rootNode.rightChild = insertNode(rootNode.rightChild, value, rightChildPositionX, rightChildPositionY,nodeDepth + 1, canvas, window)
+        rightChild_X, rightChild_Y = calculate_right_child_pos(root_x, root_y, nodeDepth + 1)
+        rootNode.rightChild = insertNode(rootNode.rightChild, value, rightChild_X, rightChild_Y,nodeDepth + 1, canvas, window)
     elif value == rootNode.value:
-        showinfo(title="Insert", message="Node already in tree")
+        showinfo(title="Insert", message="Node already in the tree")
 
     return rootNode
 
 
-def insertNodeWithoutAnimation(rootNode, value, nodeDepth):
-    if nodeDepth > MAX_DEPTH:
+def insertNode_(rootNode, value, nodeDepth):
+    #insert node without animation
+    if nodeDepth > maxDepth:
         return rootNode
     if rootNode is None:
         rootNode = Node(value)
         return rootNode
     if value < rootNode.value:
-        rootNode.leftChild = insertNodeWithoutAnimation(rootNode.leftChild, value, nodeDepth + 1)
+        rootNode.leftChild = insertNode_(rootNode.leftChild, value, nodeDepth + 1)
     elif value > rootNode.value:
-        rootNode.rightChild = insertNodeWithoutAnimation(rootNode.rightChild, value, nodeDepth + 1)
+        rootNode.rightChild = insertNode_(rootNode.rightChild, value, nodeDepth + 1)
     return rootNode
 
 
@@ -66,22 +70,20 @@ def searchTree(rootNode, value, root_x, root_y, nodeDepth, canvas, window):
         return
     
     if value < rootNode.value:
-        leftChildPositionX, leftChildPositionY = calculate_left_child_pos(root_x, root_y, nodeDepth + 1)
-        canvas.create_line(root_x, root_y, leftChildPositionX, leftChildPositionY, fill="blue", width=5)
+        leftChild_X, leftChild_Y = calculate_left_child_pos(root_x, root_y, nodeDepth + 1)
+        canvas.create_line(root_x, root_y, leftChild_X, leftChild_Y, fill="blue", width=5)
         window.update()
         sleep(0.5)
-        searchTree(rootNode.leftChild, value, leftChildPositionX, leftChildPositionY, nodeDepth + 1, canvas, window)
+        searchTree(rootNode.leftChild, value, leftChild_X, leftChild_Y, nodeDepth + 1, canvas, window)
     elif value > rootNode.value:
-        rightChildPositionX, rightChildPositionY = calculate_right_child_pos(root_x, root_y, nodeDepth + 1)
-        canvas.create_line(root_x, root_y, rightChildPositionX, rightChildPositionY, fill="blue", width=5)
+        rightChild_X, rightChild_Y = calculate_right_child_pos(root_x, root_y, nodeDepth + 1)
+        canvas.create_line(root_x, root_y, rightChild_X, rightChild_Y, fill="blue", width=5)
         window.update()
         sleep(0.5)
-        searchTree(rootNode.rightChild, value, rightChildPositionX, rightChildPositionY,nodeDepth + 1, canvas, window)
+        searchTree(rootNode.rightChild, value, rightChild_X, rightChild_Y,nodeDepth + 1, canvas, window)
     elif value == rootNode.value:
         
-        createOvalWithText(canvas, root_x, root_y,
-                            NODE_RADIUS, "red",  
-                            value, HIGHLIGHT_TEXT_COLOR, FONT_SIZE)
+        oval_with_text(canvas, root_x, root_y,node_radius, "red", value, hightlight_text, font_size)
         window.update()
         sleep(2)
 
@@ -92,102 +94,72 @@ def deleteNode(rootNode, value, rootPositionX, rootPositionY, nodeDepth, canvas,
         return None
 
     if value < rootNode.value:
+        leftChild_X, leftChild_Y = calculate_left_child_pos(rootPositionX, rootPositionY, nodeDepth + 1)
+        rootNode.leftChild = deleteNode(rootNode.leftChild, value, leftChild_X, leftChild_Y, nodeDepth + 1, canvas, window)
 
-        leftChildPositionX, leftChildPositionY = calculate_left_child_pos(rootPositionX, rootPositionY, nodeDepth + 1)
-        rootNode.leftChild = deleteNode(rootNode.leftChild, value, leftChildPositionX, leftChildPositionY, nodeDepth + 1, canvas, window)
     elif value > rootNode.value:
-        rightChildPositionX, rightChildPositionY = calculate_right_child_pos(rootPositionX, rootPositionY, nodeDepth + 1)
-        rootNode.rightChild = deleteNode(rootNode.rightChild, value, rightChildPositionX, rightChildPositionY,nodeDepth + 1, canvas, window)
+        rightChild_X, rightChild_Y = calculate_right_child_pos(rootPositionX, rootPositionY, nodeDepth + 1)
+        rootNode.rightChild = deleteNode(rootNode.rightChild, value, rightChild_X, rightChild_Y,nodeDepth + 1, canvas, window)
+
     elif value == rootNode.value:
         
         if rootNode.leftChild is None and rootNode.rightChild is None:
             return None
 
-        clearCanvasAndDrawTree()
+        clearAndDrawTree()
 
         if rootNode.rightChild is not None:
+            rightChild_X, rightChild_Y = calculate_right_child_pos(rootPositionX, rootPositionY, nodeDepth + 1)
+            rootNode.value = getMinNodeValue(rootNode.rightChild, rightChild_X, rightChild_Y, nodeDepth + 1, canvas, window)
+            clearAndDrawTree()
+            rootNode.rightChild = deleteNode(rootNode.rightChild, rootNode.value,rightChild_X, rightChild_Y,nodeDepth + 1,canvas, window)
 
-            rightChildPositionX, rightChildPositionY = calculate_right_child_pos(rootPositionX, rootPositionY, nodeDepth + 1)
-            rootNode.value = getMinNodeValue(rootNode.rightChild, 
-                                         rightChildPositionX, rightChildPositionY, 
-                                         nodeDepth + 1, 
-                                         canvas, window)
-            clearCanvasAndDrawTree()
-            rootNode.rightChild = deleteNode(rootNode.rightChild, rootNode.value,
-                                             rightChildPositionX, rightChildPositionY,
-                                             nodeDepth + 1,
-                                             canvas, window)
         else:
-            leftChildPositionX, leftChildPositionY = calculate_left_child_pos(rootPositionX, rootPositionY, nodeDepth + 1)
-            rootNode.value = getMaxNodeValue(rootNode.leftChild, 
-                                         leftChildPositionX, leftChildPositionY, 
-                                         nodeDepth + 1, 
-                                         canvas, window)
+            leftChild_X, leftChild_Y = calculate_left_child_pos(rootPositionX, rootPositionY, nodeDepth + 1)
+            rootNode.value = getMaxNodeValue(rootNode.leftChild, leftChild_X, leftChild_Y, nodeDepth + 1, canvas, window)
 
-            clearCanvasAndDrawTree()
+            clearAndDrawTree()
 
-            rootNode.leftChild = deleteNode(rootNode.leftChild, rootNode.value,
-                                             leftChildPositionX, leftChildPositionY,
-                                             nodeDepth + 1,
-                                             canvas, window)
+            rootNode.leftChild = deleteNode(rootNode.leftChild, rootNode.value,leftChild_X, leftChild_Y,nodeDepth + 1,canvas, window)
+
     return rootNode
 
 
 def getMinNodeValue(rootNode, rootPositionX, rootPositionY, nodeDepth, canvas, window):
-
     if rootNode.leftChild is None:
         return rootNode.value
     else:
-        leftChildPositionX, leftChildPositionY = calculate_left_child_pos(rootPositionX, rootPositionY, nodeDepth + 1)
-        return getMinNodeValue(rootNode.leftChild, leftChildPositionX, leftChildPositionY,  nodeDepth + 1, canvas, window)
+        leftChild_X, leftChild_Y = calculate_left_child_pos(rootPositionX, rootPositionY, nodeDepth + 1)
+        return getMinNodeValue(rootNode.leftChild, leftChild_X, leftChild_Y,  nodeDepth + 1, canvas, window)
 
     
 def getMaxNodeValue(rootNode, rootPositionX, rootPositionY, nodeDepth, canvas, window):
-
     if rootNode.rightChild is None:
         return rootNode.value
     else:
-        rightChildPositionX, rightChildPositionY = calculate_right_child_pos(rootPositionX, rootPositionY, nodeDepth + 1)
-        return getMaxNodeValue(rootNode.rightChild, rightChildPositionX, rightChildPositionY, nodeDepth + 1, canvas, window)
+        rightChild_X, rightChild_Y = calculate_right_child_pos(rootPositionX, rootPositionY, nodeDepth + 1)
+        return getMaxNodeValue(rootNode.rightChild, rightChild_X, rightChild_Y, nodeDepth + 1, canvas, window)
 
 def drawTree(rootNode, rootPositionX, rootPositionY, nodeDepth, canvas, window):
     if rootNode is None:
         return
 
     if rootNode.leftChild is not None:
-        leftChildPositionX, leftChildPositionY = calculate_left_child_pos(rootPositionX, rootPositionY, nodeDepth + 1)
-        canvas.create_line(rootPositionX, rootPositionY,
-                           leftChildPositionX, leftChildPositionY, 
-                           fill=LINE_COLOR, width=5)
-        drawTree(rootNode.leftChild, 
-                 leftChildPositionX, leftChildPositionY, 
-                 nodeDepth + 1,
-                 canvas, window)
+        leftChild_X, leftChild_Y = calculate_left_child_pos(rootPositionX, rootPositionY, nodeDepth + 1)
+        canvas.create_line(rootPositionX, rootPositionY,leftChild_X, leftChild_Y, fill=line_color, width=5)
+        drawTree(rootNode.leftChild, leftChild_X, leftChild_Y, nodeDepth + 1,canvas, window)
 
     if rootNode.rightChild is not None:
-        rightChildPositionX, rightChildPositionY = calculate_right_child_pos(rootPositionX, rootPositionY, nodeDepth + 1)
-        canvas.create_line(rootPositionX, rootPositionY,
-                           rightChildPositionX, rightChildPositionY, 
-                           fill=LINE_COLOR, width=5)
-        drawTree(rootNode.rightChild, 
-                 rightChildPositionX, rightChildPositionY, 
-                 nodeDepth + 1,
-                 canvas, window)
+        rightChild_X, rightChild_Y = calculate_right_child_pos(rootPositionX, rootPositionY, nodeDepth + 1)
+        canvas.create_line(rootPositionX, rootPositionY,rightChild_X, rightChild_Y, fill=line_color, width=5)
+        drawTree(rootNode.rightChild, rightChild_X, rightChild_Y, nodeDepth + 1,canvas, window)
 
-    createOvalWithText(canvas, rootPositionX, rootPositionY, 
-                     NODE_RADIUS, NODE_COLOR, 
-                     rootNode.value, TEXT_COLOR, FONT_SIZE)
+    oval_with_text(canvas, rootPositionX, rootPositionY, node_radius, node_color, rootNode.value, text_color, font_size)
     window.update()
 
-def clearCanvasAndDrawTree():
-        treePositionX = WINDOW_WIDTH/2
-        treePositionY = Y_PADDING
-        canvas.delete("all")
-        drawTree(rootNode, treePositionX, treePositionY, 0, canvas, window)
+def clearAndDrawTree():
+    tree_X = window_width/2
+    tree_Y = y_padding
+    canvas.delete("all")
+    drawTree(rootNode, tree_X, tree_Y, 0, canvas, window)
 
-def createOvalWithText(canvas, centerX, centerY, radius, ovalColor, text, textColor, fontSize):
-    oval = canvas.create_oval(centerX - radius, centerY - radius,
-                       centerX + radius, centerY + radius,
-                       fill=ovalColor, width=0)
-    text = canvas.create_text(centerX, centerY,
-                       text=text, fill=textColor, font=("Arial " + str(int(fontSize)) + " bold"))
